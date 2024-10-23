a8b13f338935d945323ac022f9f7e954eaec0c53
Gabriela Alonso <ealoama119@g.educaand.es>
2024-10-23 10:13:20 +0200

    primer commit

---
+++ src/es/iesoretania/proyectoeclipse/datos/Prueba.java
@@ -0,0 +1,12 @@
+package es.iesoretania.proyectoeclipse.datos; //paquete donde está incluido
+
+public class Prueba {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Hola Mundo!!!");
+	
+		System.out.println("Hola Mundo!!!");
+	}
+
+}
